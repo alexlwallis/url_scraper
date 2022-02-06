87cccfc1718c28c2e95d480890fff7c1db9a869b
Alex Wallis <aldwallis@gmail.com>
2022-02-06 14:06:18 -0800

    Cleanup and added comments

--- README.md
+++ README.md
@@ -23,11 +23,13 @@ No need to include the relative path or absolute path.
 
 ### Database
 
-If you are intending on using a PostgreSQL database as a data source then you'll need to change the connection on line 18 in __main__.py, you will also likely need to change line 151 in __main__.py to select the correct column in the correct table for your URLs.
+If you are intending on using a PostgreSQL database as a data source then you'll need to supply the database name, the user name and the password to connect to the database. You will also need to supply the column and table name to select the URLs.
 
-When this is all ready you can run:
+`python -m scrape db db_name user password column table`
 
-`python -m scrape db`
+Example:
+
+`python -m scrape db urls awallis admin url scraped_urls`
 
 ## Data Sink
 
@@ -36,3 +38,10 @@ The Data Sink here is the output.json file and this collects all the data from t
 ## Logging
 
 This module uses the logging module in Python's standard library and logging information can be found in the log.log file.
+
+### Assumptions:
+
+- Data is supposed to be outputted in valid JSONlint, so it is outputted in a file instead of a DB
+- PostgreSQL is only DB we are interested in using
+- When we are given the URL we are only scraping the URL itself, not all of the possible paths for the URL
+- Will handle redirects for the inputted URL, but not for redirects for the various links within the inputted URLs html
--- __main__.py
+++ __main__.py
@@ -11,16 +11,18 @@ import psycopg2
 import sys
 import concurrent.futures
 import os
+import validators.url
 
-MAX_THREADS = 20
-
-conn = psycopg2.connect(
-    dbname='urls', user='awallis', password='admin'
-)
 
 logging.basicConfig(filename='./scrape/log.log', encoding='utf-8', level=logging.INFO)
 
 
+MAX_THREADS = 20
+OUTPUT_FILE = "./scrape/output.json"
+
+
+# Gets the html of the url, strains for only <a> tags, parses them and check if any are
+# applicable and return objects to be converted to json
 def crawl(url):
     html = asyncio.run(get_html(url))
     # If expected return doesn't return a list the url isn't what we want
@@ -28,7 +30,9 @@ def crawl(url):
         flag = False
         obj = {}
         multiple_found = {}
+        # Only looking for <a> tags
         only_anchor = SoupStrainer("a")
+        # This now only looks through only <a> tags within html
         for link in BeautifulSoup(html, "html.parser", parse_only=only_anchor):
             if link.has_attr('href'):
                 # If url looks like an instance we'd like to extract
@@ -42,7 +46,7 @@ def crawl(url):
                     else:
                         # If new scraped link value isn't same as old value
                         if (obj[key].lower() != val.lower()):
-                            logging.warning(f"{url}: Extra {key} value found: {val}, current value is {obj[key]}") 
+                            logging.warning(f"{url}: Extra {key} value found: {val}")
                             multiple_found[key].append(val)
                             flag = True
                         pass
@@ -54,7 +58,11 @@ def crawl(url):
     else:
         return
 
-    
+
+# Used by above function, if multiple fb handles/twitter handles etc in
+# html we check if the handle found matches the URL, and if so we pick
+# that one. If not, we just choose the first option. Returns object with
+# one value per key.
 def most_accurate(url, obj):
     nobj = {}
     for option in obj:
@@ -71,70 +79,72 @@ def most_accurate(url, obj):
     return (nobj)
 
 
+# Regex for checking if the URL is a fb handle, twitter handle, google play store
+# id, ios app store id. Returns a list, or False is it fails.
 def expected_url(url):
-    # Does it have the url expected of a Twitter handle/Google Play
-    # /Facebook Page id/IOS app store id
     facebook = re.match("(?:https?:\/\/)?(?:www\.)?(?:facebook|fb|m\.facebook)\.(?:com|me)\/(?:(?:\w)*#!\/)?(?:pages\/)?(?:[\w\-]*\/)*([\w\-\.]+)(?:\/)?", url)
-    twitter = re.match("(?:https?:\/\/)?(?:www\.)?twitter.com\/(?![a-zA-Z0-9_]+\/)([a-zA-Z0-9_]+)", url)
-    google = re.match("(?:https?:\/\/)?(?:www\.)?play\.google\.com\/store\/apps\/details\?id=([a-zA-Z0-9_]+(?:\.[a-zA-Z0-9_]+)+)", url)
-    ios = re.match("(?:https?:\/\/)?(?:www\.)?apps\.apple\.com\/[a-zA-Z]+\/app\/[a-zA-Z0-9_-]+\/id([0-9_]+)", url)
-
-    # Could hardcode in facebook: facebook for facebook.com
     if facebook:
         return ["facebook", facebook.group(1)]
-    # Could hardcode in twitter: twitter for twitter.com
-    elif twitter:
+    twitter = re.match("(?:https?:\/\/)?(?:www\.)?(?:twitter|mobile\.twitter).com\/([a-zA-Z0-9_]+)(?:\/)?", url)
+    if twitter:
         return ["twitter", twitter.group(1)]
-    elif google:
+    google = re.match("(?:https?:\/\/)?(?:www\.)?play\.google\.com\/store\/apps\/details\?id=([a-zA-Z0-9_]+(?:\.[a-zA-Z0-9_]+)+)", url)
+    if google:
         return ["google", google.group(1)]
-    elif ios:
+    ios = re.match("(?:https?:\/\/)?(?:www\.)?apps\.apple\.com\/[a-zA-Z]+\/app\/[a-zA-Z0-9_-]+\/id([0-9_]+)", url)
+    if ios:
         return ["ios", ios.group(1)]
     else:
         return False
 
 
+# Adds 'http://', if https isn't included in the URL, validates the URL,
+# asynchronously runs get request for the URL (5 sec timeout) and awaits
+# the result. (Requests doesn't run async natively)
 async def get_html(url):
     # Handle poorly formed URLs
     quick_fix_url = url if url.startswith('http') else ('http://' + url)
-    loop = asyncio.get_event_loop()
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
-        "Upgrade-Insecure-Requests": "1", "DNT": "1",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate"
-    }
+    # validators.url() is a regex to check if url is valid
+    if validators.url(quick_fix_url):
+        loop = asyncio.get_event_loop()
+        # Simulate real browser
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36",
+            "Upgrade-Insecure-Requests": "1",
+            "DNT": "1",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate"
+        }
+        try:
+            # Handling timeouts
+            response = loop.run_in_executor(None, functools.partial(requests.get, quick_fix_url, headers=headers, timeout=5))
+            awaited = await response
+            return awaited.content
 
-    try: 
-        # Handling timeouts
-        response = loop.run_in_executor(None, functools.partial(requests.get, quick_fix_url, headers=headers, timeout=5)) 
-        awaited = await response
-        return awaited.content
+        except Exception as e:
+            logging.warning(f"{quick_fix_url}: {e}")
+        return False
+    else:
+        logging.warning(f"{quick_fix_url} is not a valid URL")
 
-    except Exception as e:
-        logging.warning(f"{quick_fix_url}: {e}")
-    return False
-    
 
 def create_json(json_links):
-    with open("./scrape/output.json", 'a') as outfile:
+    with open(OUTPUT_FILE, 'a') as outfile:
         json.dump(json_links, outfile, indent=4)
         outfile.write(", ")
 
 
 def starting_json_file():
-    with open("./scrape/output.json", 'a') as outfile:
+    with open(OUTPUT_FILE, 'a') as outfile:
         outfile.write("[")
 
 
-def remove_last_comma():
-    with open("./scrape/output.json", 'rb+') as filehandle:
-        filehandle.seek(-2, os.SEEK_END)
-        filehandle.truncate() 
-
-
+# Removes last comma in file and adds a ']'
 def finish_json_file():
-    with open("./scrape/output.json", 'a') as outfile:
-        outfile.write("]")
+    with open(OUTPUT_FILE, 'rb+') as outfile:
+        outfile.seek(-2, os.SEEK_END)
+        outfile.truncate()
+        outfile.write(b']')
 
 
 def get_csv_records(file):
@@ -143,29 +153,42 @@ def get_csv_records(file):
             csv_reader = csv.reader(csv_file)
             for row in csv_reader:
                 executor.map(crawl, [row[0]])
-    
 
-def get_db_records():
+
+def get_db_records(dbname, user, password, column, table):
+    conn = psycopg2.connect(dbname=dbname, user=user, password=password)
     with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
         cursor = conn.cursor()
-        cursor.execute("SELECT url FROM scraped_urls")
+        cursor.execute(f"SELECT {column} FROM {table}")
         for record in cursor:
             executor.map(crawl, [record[0]])
 
 
 if __name__ == "__main__":
-    try: 
-        type = sys.argv[1]
-        starting_json_file()
-        if type == 'db':
-            get_db_records()
-        elif type == 'csv':
-            file = sys.argv[2] 
-            get_csv_records(file)
+    try:
+        # Has at least three args
+        if len(sys.argv) > 2:
+            type = sys.argv[1]
+            starting_json_file()
+            if type == 'db':
+                print(sys.argv)
+                print(len(sys.argv))
+                try:
+                    db_name = sys.argv[2]
+                    user = sys.argv[3]
+                    password = sys.argv[4]
+                    column = sys.argv[5]
+                    table = sys.argv[6]
+                    get_db_records(db_name, user, password, column, table)
+                except Exception as e:
+                    logging.warning(f"Module has been called incorrectly, missing database arguments: ", e)
+            elif type == 'csv':
+                file = sys.argv[2]
+                get_csv_records(file)
+            else:
+                logging.warning("Module has been called incorrectly, needs additional arguments please refer to README.md")
+            finish_json_file()
         else:
-            logging.warning(f"Incorrect argument chosen, must be either 'db' or 'csv'") 
-        
-        remove_last_comma()
-        finish_json_file()
-    except Exception:
-        logging.warn("Module has been called incorrectly")
+            logging.warning("Module has been called incorrectly, missing additional arguments")
+    except Exception as e:
+        logging.warning("Module has been called incorrectly, please refer to README.md -  error: ", e)
